db584e121fd671c295d6e2443fa4c1668de7e8c3
Jake <jake.murray9010@gmail.com>
2025-01-10 17:47:10 -0600

    Wrote tcp server program - first iteration

---
+++ tcpserver.py
@@ -0,0 +1,28 @@
+# This is a simple tcp server program from Black Hat Programming by Justin Seitz and Tim Arnold
+
+import socket
+import threading
+
+IP = '0.0.0.0'
+PORT = 9998
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind ((IP, PORT))
+    server.listen(5)
+    print(f"[*] Listening on {IP}:{PORT}")
+
+    while True:
+        client, address = server.accept()
+        print(f'[*] Accepted connection fro {address[0]:address[1]}')
+        client_handler = threading.Thread(target=handle_client, arge=(client,))
+        client_handler.start()
+
+def handle_client(client_socket):
+    with client_socket as sock:
+        request = sock.recv(1024)
+        print(f'[*] Received: {request.decode("utf-8")}')
+        sock.send(b'ACK')
+
+if __name__ == '__main__':
+    main()
